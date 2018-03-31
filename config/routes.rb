dd9d57811597a2c803d760ddd60de768ecdc5972
柳田英彦 <hide@satouhidehiko-no-MacBook-Air.local>
2018-04-01 08:39:54 +0900

    ユーザ新規登録機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
 
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331223429) do
+ActiveRecord::Schema.define(version: 20180331232711) do
 
   create_table "users", force: :cascade do |t|
     t.text "email"
-    t.text "password"
     t.text "name"
     t.text "image_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

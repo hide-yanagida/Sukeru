f316e1aa8a8147ec75b84577480cadd3e6157a76
柳田英彦 <hide@satouhidehiko-no-MacBook-Air.local>
2018-05-14 06:20:54 +0900

    top画面のカード作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_action :forbid_login_user, {only: [:top]}
   def top
+    @requests_timely = Request.all.order(updated_at: :desc).limit(4)
   end
 end

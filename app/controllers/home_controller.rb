88c7b9306c5294cc79b216c7d419b7abe2e27464
柳田英彦 <hide@satouhidehiko-no-MacBook-Air.local>
2018-05-15 06:01:20 +0900

    top画面修正中

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :forbid_login_user, {only: [:top]}
   def top
-    @requests_timely = Request.all.order(updated_at: :desc).limit(4)
+    @requests_timely = Request.all.order(updated_at: :desc).limit(8)
   end
 end

c7a3535690389069fdfae882126a4e73a87386a5
柳田英彦 <hide@satouhidehiko-no-MacBook-Air.local>
2018-06-16 12:35:55 +0900

    brunch切る

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :forbid_login_user, {only: [:top]}
+  #before_action :forbid_login_user, {only: [:top]}
   def top
     @requests_timely = Request.all.order(updated_at: :desc).limit(8)
   end
